8786843989801150f0bc06899c93c80770172297
RanaMazharKhan <ranamazhar1450@gmail.com>
2026-06-25 23:23:53 +0500

    Add and route robots.txt and sitemap.xml files for Google Search indexing

--- weather_portal/urls.py
+++ weather_portal/urls.py
@@ -9,6 +9,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('google0574e06f59cbf9df.html', TemplateView.as_view(template_name='google0574e06f59cbf9df.html', content_type='text/html')),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('sitemap.xml', TemplateView.as_view(template_name='sitemap.xml', content_type='application/xml')),
     path('', include('weather.urls')),
     path('accounts/', include('accounts.urls')),
 ]
